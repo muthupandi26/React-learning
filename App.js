4a5bdc76bf501e49bb0c214cfbaf477f4b24ce73
muthupandi <p.muthupandi@tftus.com>
2023-12-24 10:55:06 +0530

    parcel using studied how to react works

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
-const heading = React.createElement("div", {id:"parent"}, React.createElement("div", {id:"child"}, [React.createElement("h1", {}, "I am h1 tag"), React.createElement("h2", {}, "I am h2 tag")]))
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+const heading = React.createElement("div", {id:"parent"}, React.createElement("div", {id:"child"}, [React.createElement("h1", {}, "React learning Muthu"), React.createElement("h2", {}, "I am h2 tag")]))
 
 console.log(heading, "heading")
 
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # React learning Hunger!!
+
+# parcel
+
+- Dev Build
+- Local Server
+- HMR - Hot Module Replacement
+- File Watching Algorithm - written in c++
+- Caching - Faster Builds
+- Image optimization
+- Minification
+- Bundling
+- Compressing
+- Consistent Hashing
+- Code splitting
+- Differential bundling- Supports old browsers
+- Diagnostic
+- Error Handling
+- Https
+- Tree shaking - remove unused code
+- Different dev & prod bundles
